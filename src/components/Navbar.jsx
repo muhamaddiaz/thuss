6af30cb218d88984240bac33d7347b01a091b276
Muhamad Diaz <muhamaddiaz10@gmail.com>
2019-05-29 08:33:15 +0700

    menambahkan navbar + stories

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,200 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import Image1 from '../assets/img/person-1.jpg'
+
+const NavbarStyle = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0 0 10px grey;
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 15px;
+  align-items: center;
+`
+
+const MyProfileStyle = styled.div`
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  img {
+    width: 100%;
+    border-radius: 50%;
+  }
+`
+
+const MyProfileText = styled.div`
+  margin-left: 10px;
+  h2 {
+    font-size: 20px;
+    color: teal;
+    margin: 0;
+  }
+  span {
+    color: grey;
+    font-size: 10px;
+  }
+`
+
+const NavbarStatusStyle = styled.ul`
+  flex-grow: 2;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  li {
+    float: left;
+  }
+`
+
+const NavbarStatusItem = styled.li`
+  margin-left: 20px;
+  h2 {
+    font-size: 15px;
+    color: teal;
+    margin: 0;
+  }
+  span {
+    color: grey;
+    font-size: 10px;
+  }
+`
+
+const NavbarPeopleStyle = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h2 {
+    font-size: 15px;
+    color: teal;
+    margin: 0;
+  }
+`
+
+const PeopleImageCol = styled.div`
+  position: relative;
+  .gradient {
+    position: absolute;
+    background: linear-gradient(90deg, rgba(255,255,255,0), rgba(255,255,255,.5));
+    z-index: 2;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+  }
+  img {
+    width: 100%;
+    border-radius: 50%;
+  }
+  div {
+    margin-left: 10px;
+  }
+`
+
+const RoundImage = styled.div`
+  display: inline-block;
+  width: ${props => props.radius || 0};
+  height: ${props => props.radius || 0};
+  border-radius: 50%;
+`
 
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <NavbarContainer />
     )
   }
+}
+
+function NavbarContainer() {
+  return (
+    <NavbarStyle>
+      <MyProfile />
+      <NavbarStatus />
+      <NavbarPeople />
+    </NavbarStyle>
+  )
+}
+
+export function MyProfile() {
+  return (
+    <MyProfileStyle>
+      <RoundImage radius="50px">
+        <img src={Image1} alt="profie-image" />
+      </RoundImage>
+      <MyProfileText>
+        <h2>
+          Jane Doe
+        </h2>
+        <span>
+          Writer
+        </span>
+      </MyProfileText>
+    </MyProfileStyle>
+  )
+}
+
+export function NavbarStatus() {
+  return (
+    <NavbarStatusStyle>
+      <NavbarStatusItem>
+        <span>
+          Claps
+        </span>
+        <h2>
+          454 / d
+        </h2>
+      </NavbarStatusItem>
+      <NavbarStatusItem>
+        <span>
+          Connections
+        </span>
+        <h2>
+          32 Publisher
+        </h2>
+      </NavbarStatusItem>
+      <NavbarStatusItem>
+        <span>
+          People
+        </span>
+        <h2>
+          154 People
+        </h2>
+      </NavbarStatusItem>
+      <NavbarStatusItem>
+        <span>
+          Total Revenue
+        </span>
+        <h2>
+          $ 2.32 / d
+        </h2>
+      </NavbarStatusItem>
+    </NavbarStatusStyle>
+  )
+}
+
+export function NavbarPeople() {
+  return (
+    <NavbarPeopleStyle>
+      <h2>
+        People
+      </h2>
+      <PeopleImageCol>
+        <div className="gradient"></div>
+        <RoundImage radius="40px">
+          <img src={Image1} alt="profie-image" />
+        </RoundImage>
+        <RoundImage radius="40px">
+          <img src={Image1} alt="profie-image" />
+        </RoundImage>
+        <RoundImage radius="40px">
+          <img src={Image1} alt="profie-image" />
+        </RoundImage>
+        <RoundImage radius="40px">
+          <img src={Image1} alt="profie-image" />
+        </RoundImage>
+      </PeopleImageCol>
+    </NavbarPeopleStyle>
+  )
 }
 
 export default Navbar
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../index.css';
 
 import { storiesOf } from '@storybook/react';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
-import Navbar from '../components/Navbar'
+import {Navbar, MyProfile, NavbarStatus, NavbarPeople} from '../components/Navbar'
 import QuickAction from '../components/QuickAction'
 import Drafts from '../components/Drafts'
 import CreateArticle from '../components/CreateArticle';
@@ -15,7 +16,11 @@ import ActivityLog from '../components/ActivityLog';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Navbar', module).add('to Navbar', () => (<Navbar />))
+storiesOf('Navbar', module)
+  .add('to Navbar', () => (<Navbar />))
+  .add('to MyProfile', () => (<MyProfile />))
+  .add('to NavbarStatus', () => (<NavbarStatus />))
+  .add('to NavbarPeople', () => (<NavbarPeople />))
 
 storiesOf('Quick Action', module).add('to QuickAction', () => (<QuickAction />))
 
